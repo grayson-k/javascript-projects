a7796885adef49c806852a7a6f9edf95639d19c4
Grayson Keaton <gkeaton23@gmail.com>
2024-06-17 13:48:46 -0800

    completed exercise

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,6 +1,16 @@
 // Declare and initialize the variables for exercise 1 here:
+let engineIndicatorLight = "red blinking";
+let spaceSuitsOn = true;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady;
+let computerStatusCode = 200;
+let shuttleSpeed = 15000;
+let fuelLevel = 21000;
+let engineTemperature = 2500;
+let commandOverride = false;
 
 // BEFORE running the code, predict what will be printed to the console by the following statements:
+// I predict it will print "engines are off"
 
 if (engineIndicatorLight === "green") {
    console.log("engines have started");
@@ -8,4 +18,50 @@ if (engineIndicatorLight === "green") {
    console.log("engines are preparing to start");
 } else {
    console.log("engines are off");
+}
+
+if (crewStatus) {
+   console.log("Crew Ready");
+} else {
+   console.log("Crew Not Ready");
+}
+
+if (computerStatusCode === 200) {
+   console.log("Please stand by. Computer is rebooting");
+} else if (computerStatusCode === 400){
+   console.log("Success! Computer online.");
+} else {
+   console.log("ALERT: Computer offline!");
+}
+
+if (shuttleSpeed > 17500) {
+   console.log("ALERT: Escape velocity reached!");
+} else if (shuttleSpeed < 8000) {
+   console.log("ALERT: Cannot maintain orbit!");
+} else {
+   console.log("Stable speed");
+}
+
+// 4. They do not predict the same since the second example uses OR || insted of AND &&
+
+// 5.
+if (fuelLevel < 1000 || engineTemperature > 3500 || engineIndicatorLight === "red blinking") {
+   console.log("ENGINE FAILURE IMMINENT!")
+} else if (fuelLevel <= 5000 || engineTemperature > 2500) {
+   console.log("Check fuel level. Engines running hot.");
+} else if (fuelLevel > 20000 && engineTemperature <= 2500) {
+   console.log("Full tank. Engines good.");
+} else if (fuelLevel > 10000 && engineTemperature <= 2500) {
+   console.log("Fuel level above 50%. Engines good.");
+} else if (fuelLevel > 5000 && engineTemperature <= 2500) {
+   console.log ("Fuel level above 25%. Engines good.");
+} else {
+   console.log("Fuel and engine status pending");
+}
+
+// 6.
+if (fuelLevel > 20000 && engineIndicatorLight !== "red blinking" || commandOverride === true ) {
+   console.log("Cleared to launch!")
+} else {
+   console.log("Launch scrubbed!")
 }
