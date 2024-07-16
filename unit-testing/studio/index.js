9e8e9b9eb51823a0355d66e27f3135aed218c975
Grayson Keaton <gkeaton23@gmail.com>
2024-07-15 21:19:18 -0400

    studio complete

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,6 +1,28 @@
 
 let launchcode = {
-  
+    organization: "nonprofit",
+    executiveDirector: "Jeff",
+    percentageCoolEmployees: 100,
+    programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+    launchOutput: function launchOutput(num) {
+        if ((num % 2 !== 0 && num % 3 !== 0 && num % 5 !== 0)) {
+            return "Rutabagas! That doesn't work.";
+        } else if (num % 2 === 0 && num % 3 === 0 && num % 5 === 0) {
+            return "LaunchCode Rocks!";
+        } else if (num % 2 === 0 && num % 3 === 0) {
+            return "LaunchCode!";
+        } else if (num % 3 === 0 && num % 5 === 0) {
+            return "Code Rocks!";
+        } else if (num % 2 === 0 && num % 5 === 0) {
+            return "Launch Rocks! (CRASH!!!!)";
+        } else if (num % 2 === 0) {
+            return "Launch!";
+        } else if (num % 3 === 0) {
+            return "Code!";
+        } else if (num % 5 === 0) {
+            return "Rocks!";
+        }
+    }
 }
 
 module.exports = launchcode;
